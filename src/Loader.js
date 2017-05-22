d1d69d871381786f9581c48604a412e7652f703f
User13 <User13>
2017-05-22 22:47:01 +0300

    Loader: Sound loading

--- src/Loader.js
+++ src/Loader.js
@@ -1,11 +1,46 @@
 "use strict";
 
-define (function() {
+define (["howler"], function(howler) {
 	var Loader = function() {
 		this.directory_tree = require('directory-tree');
+		this.onSoundLoaded = this.onSoundLoaded.bind(this);
 	};
 
 	Loader.prototype = {
+		load: function(onComplete) {
+			var tree = this.tree = this.directory_tree("pesni");
+			var result = this.result = [];
+
+			this.onComplete = onComplete;
+
+			this.count = tree.children.length;
+			this.loaded = 0;
+
+			for (var a = 0, sound; a < tree.children.length; a++) {
+				sound = new howler.Howl({
+					src: [tree.children[a].path],
+					onload: this.onSoundLoaded,
+					onloaderror: this.onSoundLoaded
+				});
+
+				result.push({
+					sound: sound,
+					file: tree.children[a]
+				});
+
+			}
+
+			console.log(tree);
+		},
+
+		onSoundLoaded: function(){
+			this.loaded++;
+			console.log(this.count, this.loaded);
+
+			if (this.onComplete&&this.loaded == this.count) {
+				this.onComplete(this.result);
+			}
+		}
 
 	};
 
--- src/Manager.js
+++ src/Manager.js
@@ -3,10 +3,13 @@
 define (["Loader"], function(Loader) {
 	var Manager = function() {
 		this.loader = new Loader;
+		this.loader.load(this.init.bind(this));
 	};
 
 	Manager.prototype = {
-
+		init: function(){
+			console.log(arguments);
+		}
 	};
 
 	return Manager;
